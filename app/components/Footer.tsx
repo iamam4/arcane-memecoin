c46d38afd5ceb454f6cc9f1293373061da1837e9
Alex <alexandre.moreau@edu.univ-eiffel.fr>
2024-11-28 00:38:04 +0100

    Update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,20 +15,18 @@ const Footer = () => {
                             {heroSection.coin_name}
                         </h3>
                         <p className="mt-2 text-gray-400">To the moon and beyond!</p>
-                    </div>
-                    
+                    </div>   
                     <div className="flex gap-6">
-                        <Link href="https://twitter.com" target='_blank' className="hover:text-[#088ec8] transition-colors">
+                        <Link href="https://x.com" target='_blank' aria-label='X website'>
                             <FaXTwitter className=" h-6 w-6 text-white" />
                         </Link>
-                        <Link href="https://pump.fun" target='_blank'  className="hover:text-[#088ec8] transition-colors">
+                        <Link href="https://pump.fun" target='_blank' aria-label='Pump Fun website'>
                             <Image
                             src="/pump.png"
                             alt="Pump Fun"
                             width={20}
                             height={20}
                             className='h-6 w-6'
-
                             />
                         </Link>
                     </div>
@@ -37,7 +35,6 @@ const Footer = () => {
                     <p>HODL Responsibly | Not Financial Advice</p>
                     <p className="mt-2">© {new Date().getFullYear()} {heroSection.coin_name}. All rights reserved.</p>
                 </div>
-
             </div>
             <div className="absolute bottom-0 left-[-125px] w-[250px] h-[200px] bg-[#395D8B] blur-[450px] opacity-70 rounded-[118px] z-[-9999]"></div>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="This website is made for $Arcane memecoin" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Arcane Coin</title>
+      </head>
       <body className={arcane.className}>
         {children}
       </body>
